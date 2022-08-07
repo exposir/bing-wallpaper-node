c836cb74177604826f05127f4fd3393b5262edfe
孟世博 <897205285@qq.com>
2022-08-07 15:32:23 +0800

    update

---
+++ map.js
@@ -0,0 +1,9 @@
+const TodyMap = {
+  img: "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+  copyright: "杭州西湖的古典中国园林 (© DANNY HU/Getty Images)",
+  title: "贴秋膘了吗？",
+};
+
+const BeforeMap = {};
+
+export { bingMap };
