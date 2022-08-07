6dc2404b03ff72fe6b4c27c16c3432177dfb172a
孟世博 <897205285@qq.com>
2022-08-07 21:05:58 +0800

    update

--- map.js
+++ map.js
@@ -1,9 +1,110 @@
+// https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp
+
+// https://cn.bing.com/th?id=OHR.NoctilucentClouds_EN-US0838966037_UHD.jpg&pid=hp&w=384&h=216&rs=1&c=4
+
 const TodyMap = {
   img: "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
-  copyright: "杭州西湖的古典中国园林 (© DANNY HU/Getty Images)",
-  title: "贴秋膘了吗？",
+  chineseCopyright: "杭州西湖的古典中国园林 (© DANNY HU/Getty Images)",
+  chineseTitle: "贴秋膘了吗？",
 };
 
-const BeforeMap = {};
+const BeforeMap = [
+  {
+    date: "2022-08-01",
+    local4kUrl: "./static/杭州西湖的古典中国园林4K.jpg",
+    localPreviewUrl: "./static/杭州西湖的古典中国园林preview.jpeg",
+    bing4kUrl:
+      "https://cn.bing.com//th?id=OHR.theBeginningofAutumn2022_ZH-CN9413449297_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    bingPreview:
+      "https://cn.bing.com//th?id=OHR.theBeginningofAutumn2022_ZH-CN9413449297_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=480&h=271",
+    chineseTitle: "贴秋膘了吗？",
+    chineseCopyright: "杭州西湖的古典中国园林 (© DANNY HU/Getty Images)",
+    chinesePreviewTitle: "杭州西湖的古典中国园林",
+  },
+  {
+    date: "2022-08-01",
+    local4kUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    localPreviewUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&pid=hp&w=480&h=271&rs=1&c=4",
+    bing4kUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    bingPreview:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&pid=hp&w=480&h=271&rs=1&c=4",
+    chineseTitle: "贴秋膘了吗？",
+    chineseCopyright: "杭州西湖的古典中国园林 (© DANNY HU/Getty Images)",
+    chinesePreviewTitle: "杭州西湖的古典中国园林2",
+  },
+  {
+    date: "2022-08-01",
+    local4kUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    localPreviewUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&pid=hp&w=480&h=271&rs=1&c=4",
+    bing4kUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    bingPreview:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&pid=hp&w=480&h=271&rs=1&c=4",
+    chineseTitle: "贴秋膘了吗？",
+    chineseCopyright: "杭州西湖的古典中国园林 (© DANNY HU/Getty Images)",
+    chinesePreviewTitle: "杭州西湖的古典中国园林3",
+  },
+  {
+    date: "2022-08-01",
+    local4kUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    localPreviewUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&pid=hp&w=480&h=271&rs=1&c=4",
+    bing4kUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    bingPreview:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&pid=hp&w=480&h=271&rs=1&c=4",
+    chineseTitle: "贴秋膘了吗？",
+    chineseCopyright: "杭州西湖的古典中国园林 (© DANNY HU/Getty Images)",
+    chinesePreviewTitle: "杭州西湖的古典中国园林4",
+  },
+  {
+    date: "2022-08-01",
+    local4kUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    localPreviewUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&pid=hp&w=480&h=271&rs=1&c=4",
+    bing4kUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    bingPreview:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&pid=hp&w=480&h=271&rs=1&c=4",
+    chineseTitle: "贴秋膘了吗？",
+    chineseCopyright: "杭州西湖的古典中国园林 (© DANNY HU/Getty Images)",
+    chinesePreviewTitle: "杭州西湖的古典中国园林4",
+  },
+  {
+    date: "2022-08-01",
+    local4kUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    localPreviewUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&pid=hp&w=480&h=271&rs=1&c=4",
+    bing4kUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    bingPreview:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&pid=hp&w=480&h=271&rs=1&c=4",
+    chineseTitle: "贴秋膘了吗？",
+    chineseCopyright: "杭州西湖的古典中国园林 (© DANNY HU/Getty Images)",
+    chinesePreviewTitle: "杭州西湖的古典中国园林4",
+  },
+  {
+    date: "2022-08-01",
+    local4kUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    localPreviewUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&pid=hp&w=480&h=271&rs=1&c=4",
+    bing4kUrl:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp",
+    bingPreview:
+      "https://cn.bing.com//th?id=OHR.NoctilucentClouds_ZH-CN4816301354_UHD.jpg&pid=hp&w=480&h=271&rs=1&c=4",
+    chineseTitle: "贴秋膘了吗？",
+    chineseCopyright: "杭州西湖的古典中国园林 (© DANNY HU/Getty Images)",
+    chinesePreviewTitle: "杭州西湖的古典中国园林4",
+  },
+];
 
-export { bingMap };
+module.exports = { TodyMap, BeforeMap };
