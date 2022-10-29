0db6841e33063ae937e630b659fea80a0e3ed1c7
孟世博 <897205285@qq.com>
2022-10-30 00:44:11 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,7 @@
-export default {
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
   title: "Bing Wallpapers",
   description: "Automatic daily collection Bing Wallpapers",
-};
+  head: [["link", { rel: "icon", type: "image/svg+xml", href: "/logo.png" }]],
+});
